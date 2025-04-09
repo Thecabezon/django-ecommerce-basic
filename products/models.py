dd93e081e3ae1ce047578e5e4f3207c1ed79c078
Thecabezon <jeffersonrojassosa150@gmail.com>
2025-04-09 14:17:07 -0500

    Creado el modelo Product y registrado en el admin

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)  # Nombre del producto
+    description = models.TextField()  # Descripción del producto
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Precio
+    image = models.ImageField(upload_to='products/')  # Imagen del producto
+    created_at = models.DateTimeField(auto_now_add=True)  # Fecha de creación
+
+    def __str__(self):
+        return self.name
